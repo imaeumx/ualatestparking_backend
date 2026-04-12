dd08f9b59dff8d33edf09f988d169e94695846db
imaeumx <paghenson.student@ua.edu.ph>
2026-04-12 18:17:03 +0800

    Configure Django admin static serving on Render

--- MyBackend/settings.py
+++ MyBackend/settings.py
@@ -46,8 +46,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # WhiteNoise for static files
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,9 +121,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py cleanup_stale_reservations --older-than-hours 24
+python manage.py collectstatic --noinput
